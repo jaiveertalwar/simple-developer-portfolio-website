c6c75b65f36c2dc9f463ebdd5228f3d478dad985
Jaiveer Talwar <72844076+jaiveertalwar@users.noreply.github.com>
2021-11-11 09:58:41 +0530

    Update data.js

--- constants/data.js
+++ constants/data.js
@@ -8,44 +8,19 @@ const userData = {
   address: "Dubai, United Arab Emirates.",
   projects: [
     {
-      title: "Tailwind Master Kit",
-      link: "https://tailwindmasterkit.com",
-      imgUrl: "/tmk.jpg",
+      title: "Gifts That Give",
+      link: "https://giftsthatgive.in",
+      imgUrl: "/gtg.png",
     },
     {
-      title: "PlaceholderTech",
-      link: "https://placeholdertech.in",
-      imgUrl: "/placeholdertech.png",
+      title: "The Pet Project",
+      link: "https://thepetproject.in",
+      imgUrl: "/tpp.png",
     },
     {
-      title: "Portfolio",
-      link: "https://manuarora.in",
-      imgUrl: "/portfolio.png",
-    },
-    {
-      title: "CovidRescue",
-      link: "https://covidrescue.co.in",
-      imgUrl: "/covidrescue.png",
-    },
-    {
-      title: "Algochurn",
-      link: "https://algochurn.com",
-      imgUrl: "/algochurn.png",
-    },
-    {
-      title: "Playground",
-      link: "https://play.placeholdertech.in",
-      imgUrl: "/playground.png",
-    },
-    {
-      title: "Feedmeback",
-      link: "https://feedmeback.vercel.app",
-      imgUrl: "/feedmeback.png",
-    },
-    {
-      title: "Devmedium",
-      link: "https://devmedium.vercel.app",
-      imgUrl: "/devmedium.png",
+      title: "NoRollr",
+      link: "https://jtlr.tech/NoRollr",
+      imgUrl: "/NoRollr.png",
     },
   ],
   about: {
