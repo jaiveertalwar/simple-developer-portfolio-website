176072aa3228a0eaadfddbc0c4f38a020db5df8c
Jaiveer Talwar <72844076+jaiveertalwar@users.noreply.github.com>
2022-01-31 09:16:51 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -42,7 +42,7 @@ export default function Navbar() {
               }`}
             >
               About{" "}
-              {router.asPath === "/" && (
+              {router.asPath === "/about" && (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
